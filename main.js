d370f086e37ca70292486d92e6c7cfe2f2154edb
Bhargava696 <74226314+Bhargava696@users.noreply.github.com>
2021-07-15 15:55:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,44 @@
-function setup(){
-    saer = createCapture(VIDEO);
-    detriounium = createCanvas(500,400);
-    detriounium.position(510,200);
-    saer.size(500,500);
-    hoodRobin = ml5.poseNet(saer,modelLoaded);
-    hoodRobin.on("pose",gotPoses);
-}
-function draw(){
-    background("#14ff80");
+cy = 0;
+cx = 0;
+han1x = 0;
+han2x = 0;
+carmionium = 0;
+ee = document.getElementById("jiok").value;
 
+function preload() {
+    font = loadFont("Roboto-MediumItalic.ttf");
 }
-function modelLoaded(){
+
+function setup() {
+    saer = createCapture(VIDEO);
+    detriounium = createCanvas(500, 400);
+    detriounium.position(510, 250);
+    saer.size(500, 500);
+    hoodRobin = ml5.poseNet(saer, modelLoaded);
+    hoodRobin.on("pose", gotPoses);
+}
+
+function draw() {
+    background("#527bf5");
+    fill("#14ff80");
+    text("text", cx, cy);
+    textSize(carmionium);
+}
+
+function modelLoaded() {
     console.log("model loaded");
 }
-function gotPoses(results){
-    if(results.length > 0){
+
+function gotPoses(results) {
+    if (results.length > 0) {
         console.log(results);
+        cx = results[0].pose.nose.x;
+        cy = results[0].pose.nose.y;
+        console.log("X = " + cx + " Y = " + cy);
+        han1x = results[0].pose.rightWrist.x;
+        han2x = results[0].pose.leftWrist.x;
+        console.log("Left wrist = " + han2x + " Right wrist is = " + han1x);
+        carmionium = floor(han2x - han1x);
+        console.log(carmionium);
     }
 }
